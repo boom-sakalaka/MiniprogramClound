bf6f1f58465166e9efdca14326c9f998531fc6da
Gu <936285702@qq.com>
2020-08-28 00:06:13 +0800

    添加音乐列表

---
+++ miniprogram/components/playlist/playlist.js
@@ -0,0 +1,21 @@
+// components/playlist/playlist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    playlist: {
+      type: Object,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+});
---
+++ miniprogram/pages/demo/demo.js
@@ -0,0 +1,76 @@
+// pages/demo/demo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    openid: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 使用 let const 定义变量 块级作用域 比较安全 省资源
+    // 定义对象用字面量的方式 
+    // 高内聚 低耦合
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then(res => {
+      this.setData({
+        openid: res.result.openid
+      })
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
