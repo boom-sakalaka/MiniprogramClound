cd84637eb609eefea5afc062e2204552edffb821
Gu <936285702@qq.com>
2020-08-29 00:02:53 +0800

    数字格式化

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -8,14 +8,43 @@ Component({
       type: Object,
     },
   },
-
+  observers: {
+    ["playlist.playCount"](count) {
+      // console.log(count);
+      // console.log(this._tranNumber(count, 2));
+      this.setData({
+        _count: this._tranNumber(count, 2)
+      })
+    },
+  },
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    _count: 0
+  },
 
   /**
    * 组件的方法列表
    */
-  methods: {},
+  methods: {
+    _tranNumber(num, point) {
+      let numStr = num.toString().split(".")[0];
+      if (numStr.length < 6) {
+        return numStr;
+      } else if (numStr.length >= 6 && numStr.length <= 8) {
+        let decimal = numStr.substring(
+          numStr.length - 4,
+          numStr.length - 4 + point
+        );
+        return parseFloat(parseInt(num / 10000) + "." + decimal) + "万";
+      } else if (numStr.length > 8) {
+        let decimal = numStr.substring(
+          numStr.length - 8,
+          numStr.length - 8 + point
+        );
+        return parseFloat(parseInt(num / 100000000) + "." + decimal) + "忆";
+      }
+    },
+  },
 });
