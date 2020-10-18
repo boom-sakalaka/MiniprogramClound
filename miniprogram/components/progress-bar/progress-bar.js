7ec460af386bebbd47a86c733524d86f3f1a7255
Gu <936285702@qq.com>
2020-10-18 20:05:03 +0800

    ssh-commit-test

--- miniprogram/components/progress-bar/progress-bar.js
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -1,112 +1,111 @@
 // components/progress-bar/progress-bar.js
 let movableAreaWidth = 0;
 let movableViewWidth = 0;
-const backgroundAudioManager = wx.getBackgroundAudioManager()
+const backgroundAudioManager = wx.getBackgroundAudioManager();
 Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-    showTime : {
-      currentTime: '00:00',
-      totalTime: '00:00'
+    showTime: {
+      currentTime: "00:00",
+      totalTime: "00:00",
     },
     movableDis: 0,
-    progress: 0
+    progress: 0,
   },
 
-  lifetimes:{
-    ready(){
-      this._getMovableDis()
-      this._bindBGMEvent()
+  lifetimes: {
+    ready() {
+      this._getMovableDis();
+      this._bindBGMEvent();
     },
   },
   /**
    * 组件的方法列表
    */
   methods: {
-    _getMovableDis(){
-      const query = this.createSelectorQuery()
-      query.select('.movable-area').boundingClientRect()
-      query.select('.movable-view').boundingClientRect()
+    _getMovableDis() {
+      const query = this.createSelectorQuery();
+      query.select(".movable-area").boundingClientRect();
+      query.select(".movable-view").boundingClientRect();
       query.exec((rect) => {
-        movableAreaWidth = rect[0].width
-        movableViewWidth = rect[1].width
+        movableAreaWidth = rect[0].width;
+        movableViewWidth = rect[1].width;
         //console.log(movableAreaWidth,movableViewWidth)
-      })
+      });
     },
     _bindBGMEvent() {
       backgroundAudioManager.onPlay(() => {
-        console.log('onPlay')
-      })
+        console.log("onPlay");
+      });
 
       backgroundAudioManager.onStop(() => {
-        console.log('onStop')
-      })
+        console.log("onStop");
+      });
 
       backgroundAudioManager.onPause(() => {
-        console.log('Pause')
-        this.triggerEvent('musicPause')
-      })
+        console.log("Pause");
+        this.triggerEvent("musicPause");
+      });
 
       backgroundAudioManager.onWaiting(() => {
-        console.log('onWaiting')
-      })
+        console.log("onWaiting");
+      });
 
       // 背景音乐可以播放的状态
       backgroundAudioManager.onCanplay(() => {
-        console.log('onCanplay')
+        console.log("onCanplay");
         //console.log(backgroundAudioManager.duration)
-        if(typeof backgroundAudioManager.duration != 'undefined'){
-          this._setTime()
-        }else{
+        if (typeof backgroundAudioManager.duration != "undefined") {
+          this._setTime();
+        } else {
           setTimeout(() => {
-            this._setTime()
-          }, 1000)
+            this._setTime();
+          }, 1000);
         }
-      })
+      });
 
       backgroundAudioManager.onTimeUpdate(() => {
-        console.log('onTimeUpdate')
-      })
+        console.log("onTimeUpdate");
+      });
 
       backgroundAudioManager.onEnded(() => {
-        console.log("onEnded")
-      })
+        console.log("onEnded");
+      });
 
       backgroundAudioManager.onError((res) => {
-        console.error(res.errMsg)
-        console.error(res.errCode)
+        console.error(res.errMsg);
+        console.error(res.errCode);
         wx.showToast({
-          title: '错误:' + res.errCode,
-        })
-      })
+          title: "错误:" + res.errCode,
+        });
+      });
     },
     _setTime() {
-      const duration = backgroundAudioManager.duration
-      const durationFmt = this._dateFormat(duration)
+      const duration = backgroundAudioManager.duration;
+      const durationFmt = this._dateFormat(duration);
       this.setData({
-        ['showTime.totalTime']: `${durationFmt.min}:${durationFmt.sec}`
-      })
+        ["showTime.totalTime"]: `${durationFmt.min}:${durationFmt.sec}`,
+      });
     },
-    _dateFormat(sec){
-      const min = Math.floor(sec / 60)
-      sec = Math.floor(sec % 60)
+    _dateFormat(sec) {
+      const min = Math.floor(sec / 60);
+      sec = Math.floor(sec % 60);
       return {
-        'min': this._parse0(min),
-        'sec': this._parse0(sec)
-      }
+        min: this._parse0(min),
+        sec: this._parse0(sec),
+      };
     },
+
     // 补零
-    _parse0(sec){
-      return sec < 10 ? '0' + sec : sec
+    _parse0(sec) {
+      return sec < 10 ? "0" + sec : sec;
     },
-  }
-})
+  },
+});
