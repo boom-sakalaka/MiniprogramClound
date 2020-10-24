bc5db0bdfb18f855ddf72e7bb83c7736292218f0
Gu <936285702@qq.com>
2020-10-25 00:15:33 +0800

    添加blog微信授权弹框和功能

--- miniprogram/pages/blog/blog.js
+++ miniprogram/pages/blog/blog.js
@@ -5,12 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    modalShow: false, // 控制底部弹出层是否显示
   },
   
   // 发布功能
   onPublish(){
-    
+    // 判断用户是否授权
+    wx.getSetting({
+      success:(res) => {
+        if(res.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success:(res) =>{
+              this.onLoginSuccess({
+                detail: res.userInfo
+              })
+            }
+          })
+        }else{
+          this.setData({
+            modalShow: true
+          })
+        }
+      }
+    })
+  },
+  onLoginSuccess(event) {
+    const detail = event.detail
+    wx.navigateTo({
+      url: `../blog-edit/blog-edit?nickName=${detail.nickName}&avatarUrl=${detail.avatarUrl}`,
+    })
+  },
+  onLoginFail() {
+    wx.showModal({
+      title: '授权用户才能发布',
+      content: ''
+    })
   },
   /**
    * 生命周期函数--监听页面加载
