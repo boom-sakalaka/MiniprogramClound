3e7183a40027095662666548c1ada81832e38258
Gu <936285702@qq.com>
2020-11-01 20:06:22 +0800

    添加自动更新方法

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    this.checkUpate()
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -35,6 +35,25 @@ App({
       this.globalData.openid = openid
       if (wx.getStorageSync(openid) == '') {
         wx.setStorageSync(openid, [])
+      }
+    })
+  },
+  checkUpate(){
+    const updateManager = wx.getUpdateManager()
+    // 检测版本更新
+    updateManager.onCheckForUpdate((res)=>{
+      if (res.hasUpdate){
+        updateManager.onUpdateReady(()=>{
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用',
+            success(res){
+              if(res.confirm){
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
       }
     })
   },
